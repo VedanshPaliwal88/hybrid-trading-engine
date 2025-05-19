d3d8dd3e6f8d17ca773ccf128f2974ba035729f7
VedanshPaliwal88 <66903973+VedanshPaliwal88@users.noreply.github.com>
2025-05-19 13:41:55 -0400

    added simulator header

---
+++ include/market_data/MarketDataSimulator.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "market_data/MarketTypes.h"
+#include <string>
+#include <vector>
+#include <random>
+#include <functional>
+
+namespace trading {
+    class MarketDataSimulator {
+    };
+}

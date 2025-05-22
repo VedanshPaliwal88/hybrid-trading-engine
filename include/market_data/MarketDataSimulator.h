79d1fd7293e67975b8355aa76d2b87193701f81a
VedanshPaliwal88 <66903973+VedanshPaliwal88@users.noreply.github.com>
2025-05-22 00:32:13 -0400

    completed sim header

--- include/market_data/MarketDataSimulator.h
+++ include/market_data/MarketDataSimulator.h
@@ -8,5 +8,28 @@
 
 namespace trading {
     class MarketDataSimulator {
+        public:
+        MarketDataSimulator(const std::string& symbol, double startPrice, double volatility = 0.001);
+
+        MarketUpdate generateUpdate();
+
+        void setUpdateCallback(std::function<void(const MarketUpdate&)> callback);
+
+        void start(int updateFrequencyMs);
+
+        void stop();
+
+
+        private:
+        std::string symbol_;
+        double currentPrice_;
+        double volatility_;
+        std::mt19937 rng_;
+        std::normal_distribution<double> priceChange_;
+        std::uniform_int_distribution<int> quantityDist_;
+        std::uniform_int_distribution<int> sideDist_;
+        std::function<void(const MarketUpdate&)> callback_;
+        bool running_;
+
     };
 }
